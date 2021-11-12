620ccf901912e7edab7ae4d7f02a4624e33dd3d1
Mihail Shaklev <60180238+mihailShaklev@users.noreply.github.com>
2021-11-12 10:32:27 +0200

    Update settings.py

--- kole/settings.py
+++ kole/settings.py
@@ -26,12 +26,12 @@ MEDIA_URL = '/furniture/'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#t*^(+l99mjmyvyv#szzc-$$x^qq$40*w$6t+s7^u_ck3481&%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ide-12493fef72c148b197921fa560fad4ad-8080.cs50.ws']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -112,8 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'noreplytothis377@gmail.com'
-EMAIL_HOST_PASSWORD= 'ggdtnufxeesuxwwj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD= ''
 EMAIL_PORT = 587
 
 
